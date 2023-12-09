1cf72ea005459d414214e1bef482dca06ba2e815
Aiden Peace <aidenpeace300@gmail.com>
2023-12-08 18:46:18 -0800

    added office to categories section.

--- src/BudgetDropdown.tsx
+++ src/BudgetDropdown.tsx
@@ -12,6 +12,7 @@ import groceriesLogo from "./images/categorieLogos/groceriesLogo.png";
 import healthcareLogo from "./images/categorieLogos/healthcareLogo.png";
 import housingLogo from "./images/categorieLogos/housingLogo.png";
 import miscellaneousLogo from "./images/categorieLogos/miscellaneousLogo.png";
+import officeLogo from "./images/categorieLogos/officeLogo.png";
 
 export default function BudgetDropdown() {
   return (
@@ -44,6 +45,7 @@ export default function BudgetDropdown() {
         <Categories logo={healthcareLogo} title="Healthcare" total={'100'} spent={'100'} color="#AF3F9D"/>
         <Categories logo={housingLogo} title="Housing" total={'1,750'} spent={'1,750'} color="#FF373C"/>
         <Categories logo={miscellaneousLogo} title="Miscellaneous" total={'50'} spent={'0'} color="#B0866D"/>
+        <Categories logo={officeLogo} title="Office" total={'20'} spent={'0'} color="#DD7298"/>
       </div>
     </>
   );
