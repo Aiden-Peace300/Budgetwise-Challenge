23b7c1a3439190b6bdae62589eb5fc843974effb
Aiden Peace <aidenpeace300@gmail.com>
2023-12-08 20:37:33 -0800

    implementing ImportantBudgetDetails.tsx

--- src/ImportantBudgetDetails.tsx
+++ src/ImportantBudgetDetails.tsx
@@ -1,10 +1,27 @@
 import './ImportantBudgetDetails.css';
 
 export default function ImportantBudgetDetails() {
+  const spent = '800';
+  const available = '1,200';
+  const budget = '2,000';
+
   return (
     <div>
       <div className="overall_container">
-        <p>spent</p>
+        <div className='spent_container'>
+          <p>Spent</p>
+          <span>{`$${spent}`}</span>
+        </div>
+        <hr className='horizontal_line' />
+        <div className='available_container'>
+          <p>Available</p>
+          <span>{`$${available}`}</span>
+        </div>
+        <hr className='horizontal_line' />
+        <div className='budget_container'>
+          <p>Budget</p>
+          <span>{`$${budget}`}</span>
+        </div>
       </div>
     </div>
   );
