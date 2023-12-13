20dd9023f39b1db80cdf67bac79e94f9a9773fc8
Aiden Peace <aidenpeace300@gmail.com>
2023-12-13 13:22:59 -0800

    added basic graphbar to SixMonthBarGraph.tsx

---
+++ src/SixMonthBarGraph.tsx
@@ -0,0 +1,45 @@
+import './SixMonthBarGraph.css'
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+)
+export default function SixMonthBarGraph() {
+
+  const data = {
+    labels: ['Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
+    datasets: [
+      {
+        lable: '369',
+        data: [1600, 1200, 1800, 1900, 1800, 1000],
+        backgroundColor: 'aqua',
+        borderColor: 'black',
+        borderWidth: 1
+      }
+    ]
+  }
+
+  const options = {
+
+  }
+  return (
+    <div className='six_month_bar_graph_container'>
+      <Bar
+        data={data}
+        options={options}
+      />
+    </div>
+  );
+}
